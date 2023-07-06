e7613d2e36d820c78fc46632d814c347e3f3aa44
Dominika Hürtig <dominikahurtig@Dnia.local>
2023-07-06 06:53:54 +0200

    thursday 06.07.2023

--- lol.py
+++ lol.py
@@ -93,9 +93,9 @@ print(y)
 print(z)
 
 #Python print() function is often used to output variables. In the print() function, you output multiple variables, separated by a comma. 
-#You can also use the + operator to output multiple variables. For numbers, the + character works as a mathematical operator.
-#In the print() function, when you try to combine a string and a number with the + operator, Python will give you an error.
-#The best way to output multiple variables in the print() function is to separate them with commas, which even support different data types.
+  #You can also use the + operator to output multiple variables. For numbers, the + character works as a mathematical operator.
+    #In the print() function, when you try to combine a string and a number with the + operator, Python will give you an error.
+      #The best way to output multiple variables in the print() function is to separate them with commas, which even support different data types.
 
 x = "Python is awesome"
 print(x)
@@ -120,7 +120,7 @@ print(x, y)
 
 
 #Variables that are created outside of a function (as in all of the examples above) are known as global variables.
-#Global variables can be used by everyone, both inside of functions and outside.
+  #Global variables can be used by everyone, both inside of functions and outside.
 
 x = "awesome"
 
@@ -131,7 +131,7 @@ myfunc()
 
 
 #If you create a variable with the same name inside a function, this variable will be local, and can only be used inside the function.
-#The global variable with the same name will remain as it was, global and with the original value.
+  #The global variable with the same name will remain as it was, global and with the original value.
 
 x = "awesome"
 
@@ -145,7 +145,7 @@ print("Python is " + x)
 
 
 #Normally, when you create a variable inside a function, that variable is local, and can only be used inside that function.
-#To create a global variable inside a function, you can use the global keyword.
+  #To create a global variable inside a function, you can use the global keyword.
 
 
 def myfunc():
@@ -171,8 +171,8 @@ print("Python is " + x)
 
 
 #In programming, data type is an important concept.
-#Variables can store data of different types, and different types can do different things.
-#Python has the following data types built-in by default, in these categories
+  #Variables can store data of different types, and different types can do different things. 
+    #Python has the following data types built-in by default, in these categories
 
 
 """
@@ -192,6 +192,131 @@ NONE TYPE: NoneType
 x = 5
 print(type(x))
 
+#Three numeric types: 
+  #int
+    #float
+      #complex
 
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+
+#To verify the type of any object in Python, use the type() function.
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+#INT
+  #Int, or integer, is a whole number, positive or negative, without decimals, of unlimited length.
+
+x = 1
+y = 35656222554887711
+z = -3255522
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+#FLOAT
+  #Float, or "floating point number" is a number, positive or negative, containing one or more decimals.
+
+x = 1.10
+y = 1.0
+z = -35.59
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+      #Float can also be scientific numbers with an "e" to indicate the power of 10.
+
+x = 35e3
+y = 12E4
+z = -87.7e100
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+#COMPLEX
+  #Complex numbers are written with a "j" as the imaginary part.
+
+x = 3+5j
+y = 5j
+z = -5j
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+#You can convert from one type to another with the int(), float(), and complex() methods.
+  #Note: You cannot convert complex numbers into another number type.
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+#convert from int to float:
+a = float(x)
+
+#convert from float to int:
+b = int(y)
+
+#convert from int to complex:
+c = complex(x)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+
+#Python does not have a random() function to make a random number, 
+  #but Python has a built-in module called random that can be used to make random numbers.
+
+
+import random
+
+print(random.randrange(1, 10))
+
+#There may be times when you want to specify a type on to a variable. This can be done with casting. 
+  #Python is an object-orientated language, and as such it uses classes to define data types, including its primitive types.
+    #Casting in python is therefore done using constructor functions.
+  #int() - constructs an integer number from an integer literal, 
+    #a float literal (by removing all decimals), or a string literal (providing the string represents a whole number)
+  #float() - constructs a float number from an integer literal, 
+      #a float literal or a string literal (providing the string represents a float or an integer)
+  #str() - constructs a string from a wide variety of data types, including strings, integer literals and float literals
+
+#INT:
+
+x = int(1)   # x will be 1
+y = int(2.8) # y will be 2
+z = int("3") # z will be 3
+
+#FLOATS
+
+x = float(1)     # x will be 1.0
+y = float(2.8)   # y will be 2.8
+z = float("3")   # z will be 3.0
+w = float("4.2") # w will be 4.2
+
+#STRINGS
+
+x = str("s1") # x will be 's1'
+y = str(2)    # y will be '2'
+z = str(3.0)  # z will be '3.0'
+
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
 
 
