35c668ef3d4ab524cbbc386dff81a293d4b3c3bd
Dominika Hürtig <dominikahurtig@Dnia.local>
2023-08-01 09:11:29 +0200

    01-08-2023

--- lol.py
+++ lol.py
@@ -419,8 +419,21 @@ print(a.replace("World", "Earth"))
 a = "Hello, World!"
 print(a.split(","))
 
+#To concatenate, or combine, two strings you can use the + operator.
 
+#Merge variable a with variable b into variable c:
 
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
+
+#To add a space between them, add a " ":
+
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c)
 
 
 
