48e7abb891d4a5f8733f594f73a7bfd4e5579e04
Dominika Hürtig <photowithmagic@gmail.com>
2023-08-02 18:03:38 +0200

    Adding learning

---
+++ Learning.py
@@ -0,0 +1,19 @@
+print ('Hello to anyone in the world')
+
+#small glucose level check code:
+# Glucose level is an input for this software
+glucose_level = int(input())
+
+# Display message if glucose level is lower than 70
+if glucose_level < 70:
+    print("Low Glucose Level")
+
+# Display message if Glucose level is higher than 170
+
+elif glucose_level > 170:
+    print("High Glucose Level")
+
+#Display message if none of the above conditions are met
+
+else:
+    print("Normal Glucose Level")
