707b1a81933552312200f3f035953d027ecd5c5e
Dominika Hürtig <photowithmagic@gmail.com>
2023-08-03 10:54:58 +0200

    new file - task3

---
+++ task3.py
@@ -0,0 +1,12 @@
+# The upcoming code takes text data as input.
+# Write the code to convert it to uppercase and display it on the screen.
+
+text = input()
+
+#convert to uppercase
+
+upper_text = text.upper()
+
+#display on the screen
+
+print(upper_text)
