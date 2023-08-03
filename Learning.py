d696d526c28495ddd630aedad7a80cc5db6b2d3a
Dominika Hürtig <photowithmagic@gmail.com>
2023-08-03 14:00:40 +0200

    03.08.2023

--- Learning.py
+++ Learning.py
@@ -16,3 +16,11 @@ print(data.get(a, "Not found"))
 
 fib = {1: 1, 2: 1, 3: 2, 4: 3}
 print(fib.get(4, 0) + fib.get(7, 5))
+
+
+n = [2, 4, 6, 8]
+res = 1
+for x in n[1:3]:
+  res *= x
+
+print(res)
--- define-own-function.py
+++ define-own-function.py
@@ -7,9 +7,3 @@ def greet():
 greet() 
 
 
-n = [2, 4, 6, 8]
-res = 1
-for x in n[1:3]:
-  res *= x
-
-print(res)
