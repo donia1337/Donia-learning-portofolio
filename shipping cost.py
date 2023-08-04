49b000acb9ef4db7321150bfe452c3ebe04d1acb
Dominika Hürtig <photowithmagic@gmail.com>
2023-08-04 08:49:15 +0200

    changed shipping cost code

--- shipping cost.py
+++ shipping cost.py
@@ -1,6 +1,9 @@
 # Cost per kg is 5.
 # Complete the shipping_cost() function to take the weight as an argument, calculate the shipping cost based on the weight, and display it in the given format.
 
+print("Shipping cost depends on the weight. Cost per kg is 5dkk.")
+print("Please enter weight of the package")
+
 #taking the weight as input
 weight = int(input())
 
@@ -9,4 +12,7 @@ def shipping_cost(weight):
   print(weight*5)   
 
 #function call
+
+print("Your shipping cost is: ") 
 shipping_cost(weight)
+
