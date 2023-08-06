e039b90ff65b04411b5312154cf38f79b0796664
Dominika Hürtig <photowithmagic@gmail.com>
2023-08-06 19:26:13 +0200

    practicing string concatenation

---
+++ Python/string-concatenation.py
@@ -0,0 +1,11 @@
+# Little practice with string concatenation (how to put strings together).
+# Let say we want to create a string that say: "All the dogs are ______".
+
+dogs = "lovely and beautiful"
+
+# We can do this in few different ways:
+
+print("All the dogs are " + dogs)
+print("All the dogs are {}".format(dogs))
+print(f"All the dogs are {dogs}")
+
