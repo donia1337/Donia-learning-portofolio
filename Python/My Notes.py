c02b95d7ab50b0578aa25215529fc3e5b0a9d762
Dominika Hürtig <photowithmagic@gmail.com>
2023-08-17 16:29:07 +0200

    tuples

--- Python/My Notes.py
+++ Python/My Notes.py
@@ -961,6 +961,63 @@ sort()	Sorts the list
 """
 
 
+# Python Tuples
+
+mytuple = ("apple", "banana", "cherry")
+
+# Tuples are used to store multiple items in a single variable.
+# Tuple is one of 4 built-in data types in Python used to store collections of data, 
+# the other 3 are List, Set, and Dictionary, all with different qualities and usage.
+# A tuple is a collection which is ordered and unchangeable.
+# Tuples are written with round brackets.
+
+thistuple = ("apple", "banana", "cherry")
+print(thistuple)
+
+# Tuple items are ordered, unchangeable, and allow duplicate values.
+# Tuple items are indexed, the first item has index [0] etc.
+# When we say that tuples are ordered, it means that the items have a defined order, and that order will not change.
+# Tuples are unchangeable, meaning that we cannot change, add or remove items after the tuple has been created.
+# Since tuples are indexed, they can have items with the same value:
+
+thistuple = ("apple", "banana", "cherry", "apple", "cherry")
+print(thistuple)
+# To determine how many items a tuple has, use the len() function:
+
+thistuple = ("apple", "banana", "cherry")
+print(len(thistuple))
+
+# To create a tuple with only one item, you have to add a comma after the item, otherwise Python will not recognize it as a tuple.
+
+thistuple = ("apple",)
+print(type(thistuple))
+
+# NOT a tuple
+thistuple = ("apple")
+print(type(thistuple))
+
+# Tuple items can be of any data type:
+
+tuple1 = ("apple", "banana", "cherry")
+tuple2 = (1, 5, 7, 9, 3)
+tuple3 = (True, False, False)
+
+# A tuple can contain different data types:
+
+tuple1 = ("abc", 34, True, 40, "male")
+
+# It is also possible to use the tuple() constructor to make a tuple.
+
+thistuple = tuple(("apple", "banana", "cherry")) # note the double round-brackets
+print(thistuple)
+
+"""
+List - is a collection which is ordered and changeable. Allows duplicate members.
+Tuple - is a collection which is ordered and unchangeable. Allows duplicate members.
+Set - is a collection which is unordered, unchangeable*, and unindexed. No duplicate members.
+Dictionary - is a collection which is ordered** and changeable. No duplicate members.
+"""
+
 
 
 
