edb07d0d079cb12103db0f1448278384eb0c8785
Dominika Hürtig <photowithmagic@gmail.com>
2023-08-17 16:48:34 +0200

    more tuples

--- Python/My Notes.py
+++ Python/My Notes.py
@@ -1018,6 +1018,93 @@ Set - is a collection which is unordered, unchangeable*, and unindexed. No dupli
 Dictionary - is a collection which is ordered** and changeable. No duplicate members.
 """
 
+# Access typles by index
+
+thistuple = ("apple", "banana", "cherry")
+print(thistuple[1])
+
+# Negative indexing means start from the end.
+# -1 refers to the last item, -2 refers to the second last item etc.
+
+thistuple = ("apple", "banana", "cherry")
+print(thistuple[-1])
+
+# You can specify a range of indexes by specifying where to start and where to end the range.
+# When specifying a range, the return value will be a new tuple with the specified items.
+
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[2:5])
+
+# By leaving out the start value, the range will start at the first item:
+
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[:4])
+
+# By leaving out the end value, the range will go on to the end of the list:
+
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[2:])
+
+# Specify negative indexes if you want to start the search from the end of the tuple:
+
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[-4:-1])
+
+# To determine if a specified item is present in a tuple use the in keyword:
+
+thistuple = ("apple", "banana", "cherry")
+if "apple" in thistuple:
+  print("Yes, 'apple' is in the fruits tuple")
+
+
+# Once a tuple is created, you cannot change its values. Tuples are unchangeable, or immutable as it also is called.
+# But there is a workaround. You can convert the tuple into a list, change the list, and convert the list back into a tuple.
+
+x = ("apple", "banana", "cherry")
+y = list(x)
+y[1] = "kiwi"
+x = tuple(y)
+
+print(x)
+
+# Since tuples are immutable, they do not have a built-in append() method, but there are other ways to add items to a tuple.
+# 1. Convert into a list: Just like the workaround for changing a tuple, 
+# you can convert it into a list, add your item(s), and convert it back into a tuple.
+
+thistuple = ("apple", "banana", "cherry")
+y = list(thistuple)
+y.append("orange")
+thistuple = tuple(y)
+
+# 2. Add tuple to a tuple. You are allowed to add tuples to tuples, 
+# so if you want to add one item, (or many), create a new tuple with the item(s), and add it to the existing tuple:
+
+thistuple = ("apple", "banana", "cherry")
+y = ("orange",)
+thistuple += y
+
+print(thistuple)
+
+# Remove items in tuples
+# Tuples are unchangeable, 
+# so you cannot remove items from it, but you can use the same workaround as we used for changing and adding tuple items:
+
+thistuple = ("apple", "banana", "cherry")
+y = list(thistuple)
+y.remove("apple")
+thistuple = tuple(y)
+
+# Or you can delete the tuple completely:
+# The del keyword can delete the tuple completely:
+
+thistuple = ("apple", "banana", "cherry")
+del thistuple
+print(thistuple) #this will raise an error because the tuple no longer exists
+
+
+
+
+
 
 
 
