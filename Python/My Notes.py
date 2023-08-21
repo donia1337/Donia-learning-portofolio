bd9a9fa6411769fc78cc35409a9f07f9ffb6ceec
Dominika Hürtig <photowithmagic@gmail.com>
2023-08-21 16:59:56 +0200

    typle methods

--- Python/My Notes.py
+++ Python/My Notes.py
@@ -1154,6 +1154,37 @@ for i in range(len(thistuple)):
   print(thistuple[i])
 
 
+# You can loop through the tuple items by using a while loop.
+# Use the len() function to determine the length of the tuple, t
+# hen start at 0 and loop your way through the tuple items by referring to their indexes.
+# Remember to increase the index by 1 after each iteration.
+
+thistuple = ("apple", "banana", "cherry")
+i = 0
+while i < len(thistuple):
+  print(thistuple[i])
+  i = i + 1
+
+# To join two or more tuples you can use the + operator:
+
+tuple1 = ("a", "b" , "c")
+tuple2 = (1, 2, 3)
+
+tuple3 = tuple1 + tuple2
+print(tuple3)
+
+# If you want to multiply the content of a tuple a given number of times, you can use the * operator:
+
+fruits = ("apple", "banana", "cherry")
+mytuple = fruits * 2
+
+print(mytuple)
+
+"""
+  Tuple Method
+count()	Returns the number of times a specified value occurs in a tuple
+index()	Searches the tuple for a specified value and returns the position of where it was found
+"""
 
 
 
