08ced92dd3f03122fbae7ed0dee39c09b04fd2ad
Dominika Hürtig <photowithmagic@gmail.com>
2023-08-22 08:58:37 +0200

    Band Name Generator

---
+++ Python/Small projects/Band Name Generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator! Here you can get some ideas for your Band Name.")
+town = (input("What's the name of the City you grw up in?"))
+animal = (input("What's the name of your animal?"))
+print("Here the idea for your Band Name: " + town + " " + animal)
