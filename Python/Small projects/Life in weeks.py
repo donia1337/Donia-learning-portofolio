e0771e8f3eb1323c8cd9474f9e079d4daedfead7
Dominika Hürtig <photowithmagic@gmail.com>
2023-08-24 14:09:57 +0200

    Life in weeks

---
+++ Python/Small projects/Life in weeks.py
@@ -0,0 +1,13 @@
+print("Let's say that you will live 90 years.\nDo you want to know how many months, weeks and days you have left?\nLet's find out!")
+
+
+age = input("What is your current age? ")
+
+years = 90 - int(age)
+months = round(years * 12)
+weeks = round(years * 52)
+days = round(years * 365)
+
+print(f"You have {days} days, {weeks} weeks, and {months} months left.")
+
+
