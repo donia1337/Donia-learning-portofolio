2a036e61216ffc927a4b5b74578f90134162a256
Dominika Hürtig <photowithmagic@gmail.com>
2023-08-29 16:54:04 +0200

    adding and removing objects from sets

--- Python/My Notes.py
+++ Python/My Notes.py
@@ -1244,7 +1244,62 @@ print(thisset)
 # You cannot access items in a set by referring to an index or a key.
 # But you can loop through the set items using a for loop, or ask if a specified value is present in a set, by using the in keyword.
 
+thisset = {"apple", "banana", "cherry"}
 
+for x in thisset:
+  print(x)
+
+# Check if "banana" is present in the set:
+
+thisset = {"apple", "banana", "cherry"}
+
+print("banana" in thisset)
+
+# To add one item to a set use add() method
+
+thisset = {"apple", "banana", "cherry"}
+
+thisset.add("orange")
+
+print(thisset)
+
+
+# To add items from another set into the current set, use the update() method.
+
+thisset = {"apple", "banana", "cherry"}
+tropical = {"pineapple", "mango", "papaya"}
+
+thisset.update(tropical)
+
+print(thisset)
+
+
+# The object in the update() method does not have to be a set, it can be any iterable object (tuples, lists, dictionaries etc.).
+
+thisset = {"apple", "banana", "cherry"}
+mylist = ["kiwi", "orange"]
+
+thisset.update(mylist)
+
+print(thisset)
+
+# To remove an item in a set, use the remove(), or the discard() method.
+# If the item to remove does not exist, remove() will raise an error.
+
+
+thisset = {"apple", "banana", "cherry"}
+
+thisset.remove("banana")
+
+print(thisset)
+
+# If the item to remove does not exist, discard() will NOT raise an error.
+
+thisset = {"apple", "banana", "cherry"}
+
+thisset.discard("banana")
+
+print(thisset)
 
 
 
