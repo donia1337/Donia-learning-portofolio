a07483d0eca0e0a5512b2f4a23266644754ec7d3
Dominika Hürtig <photowithmagic@gmail.com>
2023-08-30 10:34:05 +0200

    f-Strings

---
+++ Python/Chapter 1/fStrings.py
@@ -0,0 +1,11 @@
+# You can insert a variable into a string by using f-strings.
+# The syntax is simple, just insert the variable in-between a set of curly braces {}.
+
+# for example:
+
+
+score = 1
+height = 1.9
+isWinning = True
+
+print(f"Your score is {score}, your height is {height}, you are winning {isWinning}")
