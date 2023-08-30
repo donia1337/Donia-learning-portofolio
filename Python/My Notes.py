37b1384c89dd1d647438d1fc430619f244caae76
Dominika Hürtig <photowithmagic@gmail.com>
2023-08-30 10:23:25 +0200

    update

--- Python/My Notes.py
+++ Python/My Notes.py
@@ -1301,6 +1301,84 @@ thisset.discard("banana")
 
 print(thisset)
 
+# You can also use the pop() method to remove an item, 
+# but this method will remove a random item, so you cannot be sure what item that gets removed.
+# The return value of the pop() method is the removed item.
+
+thisset = {"apple", "banana", "cherry"}
+
+x = thisset.pop()
+
+print(x)
+
+print(thisset)
+
+# The clear() method empties the set:
+
+thisset = {"apple", "banana", "cherry"}
+
+thisset.clear()
+
+print(thisset)
+
+
+# The del keyword will delete the set completely:
+
+thisset = {"apple", "banana", "cherry"}
+
+del thisset
+
+print(thisset)
+
+
+# You can loop through the set items by using a for loop.
+
+thisset = {"apple", "banana", "cherry"}
+
+for x in thisset:
+  print(x)
+
+
+# There are several ways to join two or more sets in Python.
+# You can use the union() method that returns a new set containing all items from both sets, 
+# or the update() method that inserts all the items from one set into another
+
+set1 = {"a", "b" , "c"}
+set2 = {1, 2, 3}
+
+set3 = set1.union(set2)
+print(set3)
+
+# The update() method inserts the items in set2 into set1:
+
+set1 = {"a", "b" , "c"}
+set2 = {1, 2, 3}
+
+set1.update(set2)
+print(set1)
+
+# Both union() and update() will exclude any duplicate items.
+
+# KEEP ONLY THE DUPLICATES
+# The intersection_update() method will keep only the items that are present in both sets.
+# The intersection() method will return a new set, that only contains the items that are present in both sets.
+
+x = {"apple", "banana", "cherry"}
+y = {"google", "microsoft", "apple"}
+
+x.intersection_update(y)
+
+print(x)
+
+x = {"apple", "banana", "cherry"}
+y = {"google", "microsoft", "apple"}
+
+z = x.intersection(y)
+
+print(z)
+
+# Keep All, But NOT the Duplicates
+# The symmetric_difference_update() method will keep only the elements that are NOT present in both sets.
 
 
 
