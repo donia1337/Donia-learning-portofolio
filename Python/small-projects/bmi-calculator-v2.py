11219b8c91b9d58ff9147bcd009759440290f119
Dominika Hürtig <photowithmagic@gmail.com>
2023-09-01 08:23:18 +0200

    bmi calculator v2

---
+++ Python/small-projects/bmi-calculator-v2.py
@@ -0,0 +1,20 @@
+print("Here is our BMI calculator. Please provide information below.")
+
+
+height = float(input("enter your height in metres: "))
+weight = float(input("enter your weight in kilograms: "))
+
+bmi =round(weight / height ** 2)
+
+if bmi < 18.5:
+    print(f"Your BMI is {bmi}, you are underweight.")
+elif bmi < 25:
+    print(f"Your BMI is {bmi}, you are in normal weight range.")
+elif bmi < 30:
+    print(f"Your BMI is {bmi}, you are slightly overweight.")
+elif bmi < 35:
+    print(f"Your BMI is {bmi}, you are obese.")
+else:
+    print(f"Your BMI is {bmi}, you are clinically obese.")
+
+
