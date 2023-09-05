21731e380bdfd5cfd0079c1b33d809f9b3c786de
Dominika Hürtig <photowithmagic@gmail.com>
2023-09-05 09:50:50 +0200

    dictionaries training

--- Python/my-notes.py
+++ Python/my-notes.py
@@ -1534,6 +1534,85 @@ print(x) #after the change
 x = thisdict.values()
 
 
+# Add a new item to the original dictionary, and see that the values list gets updated as well:
+
+car = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+
+x = car.values()
+
+print(x) #before the change
+
+car["color"] = "red"
+
+print(x) #after the change
+
+
+# The items() method will return each item in a dictionary, as tuples in a list.
+
+x = thisdict.items()
+
+
+# The returned list is a view of the items of the dictionary, 
+# meaning that any changes done to the dictionary will be reflected in the items list.
+
+
+car = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+
+x = car.items()
+
+print(x) #before the change
+
+car["year"] = 2020
+
+print(x) #after the change
+
+
+# Add a new item to the original dictionary, and see that the items list gets updated as well:
+
+
+car = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+
+x = car.items()
+
+print(x) #before the change
+
+car["color"] = "red"
+
+print(x) #after the change
+
+# To determine if a specified key is present in a dictionary use the in keyword:
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+if "model" in thisdict:
+  print("Yes, 'model' is one of the keys in the thisdict dictionary")
+
+
+# You can change the value of a specific item by referring to its key name:
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict["year"] = 2018
+
+
 
 
 
--- Python/testing.py
+++ Python/testing.py
@@ -1,1 +1,13 @@
-print("Hello " + input("What is your name? " ))
+car = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+
+#x = car.items()
+
+print(x) #before the change
+
+car["color"] = "red"
+
+print(x) #after the change
