db5eaf4d49659d431f9b6bb0ffa9db97388518cb
Dominika Hürtig <photowithmagic@gmail.com>
2023-09-05 11:07:09 +0200

    notes update

--- Python/my-notes.py
+++ Python/my-notes.py
@@ -1612,6 +1612,94 @@ thisdict = {
 }
 thisdict["year"] = 2018
 
+# The update() method will update the dictionary with the items from the given argument.
+# The argument must be a dictionary, or an iterable object with key:value pairs.
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.update({"year": 2020})
+
+
+# Adding an item to the dictionary is done by using a new index key and assigning a value to it:
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict["color"] = "red"
+print(thisdict)
+
+
+# There are several methods to remove items from a dictionary:
+# The pop() method removes the item with the specified key name:
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.pop("model")
+print(thisdict)
+
+# The popitem() method removes the last inserted item (in versions before 3.7, a random item is removed instead):
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.popitem()
+print(thisdict)
+
+# The del keyword removes the item with the specified key name:
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+del thisdict["model"]
+print(thisdict)
+
+# The del keyword can also delete the dictionary completely:
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+del thisdict
+print(thisdict) #this will cause an error because "thisdict" no longer exists.
+
+# The clear() method empties the dictionary:
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.clear()
+print(thisdict)
+
+# You can loop through a dictionary by using a for loop.
+
+for x in thisdict:
+  print(x)
+
+# Print all values in the dictionary, one by one:
+
+for x in thisdict:
+  print(thisdict[x])
+
+# You can also use the values() method to return values of a dictionary:
+
+for x in thisdict.values():
+  print(x)
+
 
 
 
