163ca562902e0eebc7cffb665fdc6ac134becad4
Dominika Hürtig <photowithmagic@gmail.com>
2023-09-07 07:35:30 +0200

    Random modules

---
+++ Python/small-projects/random-modules.py
@@ -0,0 +1,14 @@
+# Random integer number between 1 and 100.
+import random
+random_integer = random.randint(1, 100)
+
+print(random_integer)
+
+# Random float number between 0 and 1.
+
+random_float = random.random()
+print(random_float)
+
+
+random_decimal = random.uniform(0, 5)
+print(random_decimal)
