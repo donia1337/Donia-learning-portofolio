a2d59aa26f6a399b3b386655d2c2dfe8612cbf04
Dominika Hürtig <photowithmagic@gmail.com>
2023-09-07 16:58:43 +0200

    treasure island

---
+++ Python/chapter-1/nested-lists.py
@@ -0,0 +1,6 @@
+fruits = ["Strawberries", "Nectrarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+
+dirty_dozen = [fruits, vegetables]
+
+print(dirty_dozen)
