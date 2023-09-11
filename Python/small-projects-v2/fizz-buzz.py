f785617ae43d9dd5dd9d7f458fa45a8fdf90efb9
Dominika Hürtig <photowithmagic@gmail.com>
2023-09-11 11:04:45 +0200

    password generator

--- Python/small-projects-v2/fizz-buzz.py
+++ Python/small-projects-v2/fizz-buzz.py
@@ -1,3 +1,6 @@
+
+
+
 for number in range(1, 101):
   if number % 3 == 0 and number % 5 == 0:
     print("FizzBuzz")
@@ -7,5 +10,7 @@ for number in range(1, 101):
     print("Buzz")
   else:
     print(number)
+
+
 
 
