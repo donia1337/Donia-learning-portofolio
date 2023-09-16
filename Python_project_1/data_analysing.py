30f40f5e6e262a71df9be1791b09ffa0da24bec5
Andreas Hürtig <s145417@student.dtu.dk>
2023-09-16 09:24:16 +0200

    Template of new python project

---
+++ Python_project_1/data_analysing.py
@@ -0,0 +1,6 @@
+# This python script should handle the data analysing of the JSON file with all of our retrieved data.
+# Look at https://www.geeksforgeeks.org/read-json-file-using-python/ to figure out how!
+# https://stackoverflow.com/questions/16129652/accessing-json-elements might also be good!
+
+# We want to be able to read the elements in the JSON file, and handle the data. Thereafter show it with either a picture or a graph.
+# An example could be to use to numerical values of a matrix and convert them into a picture, turning the numerical values into color values.
---
+++ Python_project_1/read_website.py
@@ -0,0 +1,5 @@
+# This script should be able to read a website and save an url that holds json data.
+# For this example we are using: https://www.swpc.noaa.gov/products/aurora-30-minute-forecast
+
+# We could for now ignore this script and just always use the "latest" data source which would be at:
+# https://services.swpc.noaa.gov/json/ovation_aurora_latest.json
