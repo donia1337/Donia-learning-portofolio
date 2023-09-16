15d970b6b05494d40948a74c3c469e10217b005f
Dominika Hürtig <photowithmagic@gmail.com>
2023-09-16 16:22:57 +0200

    read website

--- Python_project_1/read_website.py
+++ Python_project_1/read_website.py
@@ -6,3 +6,16 @@
 
 # We can also take the data in HPI format directly from:
 # https://services.swpc.noaa.gov/text/aurora-nowcast-hemi-power.txt
+
+
+import urllib.request
+
+data = urllib.request.urlopen("https://services.swpc.noaa.gov/text/aurora-nowcast-hemi-power.txt")
+for line in data:
+    print (line)
+
+
+
+#data = urllib.request.urlopen("https://services.swpc.noaa.gov/json/ovation_aurora_latest.json")
+#for line in data:
+#    print (line)
